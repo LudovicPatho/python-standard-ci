c8f5e0e2fb0435dfb75dac269901cdb3d3e6f2d7
Patho Ludovic <ludovic.patho@gmail.com>
2023-11-23 13:28:13 +0100

    Update wallet.py

--- src/wallet.py
+++ src/wallet.py
@@ -8,6 +8,7 @@ class Wallet(object):
         self.balance = initial_amount
 
     def spend_cash(self, amount):
+        # This a comment
         if self.balance < amount:
             raise InsufficientAmount('Not enough available to spend {}'.format(amount))
         self.balance -= amount
